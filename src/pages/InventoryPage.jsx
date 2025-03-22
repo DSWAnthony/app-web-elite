681a1be9636cc1492af5c6def1e75a576d092fe9
DSWAnthony <anthonysistemas159@gmail.com>
2025-03-22 09:38:15 -0500

    updated

--- src/pages/InventoryPage.jsx
+++ src/pages/InventoryPage.jsx
@@ -1,10 +1,18 @@
 import React, { useEffect } from 'react'
 import ProductTable from '../components/product/ProductTable'
+import { getInventario } from '../services/dashboard/inventarioService'
 
 const ProductPage = () => {
   
 
   useEffect(() => {
+
+    const fetchData = async () => {
+      const inventario = await getInventario();
+    }
+
+    fetchData();
+
   }, [])
   
 
